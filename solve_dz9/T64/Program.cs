47d67b7dfd5b0c6652f63c48702eb76c7299b593
Maria <kalelm@mail.ru>
2022-10-05 21:17:23 +0300

    dz9

---
+++ solve_dz9/T64/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке 
+//от N до 1. Выполнить с помощью рекурсии.
+//N = 5 -> "5, 4, 3, 2, 1"
+//N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+
+int N = Prompt("Задайте N ");
+WriteNaturalNumbers(N);
+
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    return Convert.ToInt32(value);
+}
+
+void WriteNaturalNumbers(int N)
+{
+    System.Console.Write(N+" ");
+    if (N > 1) WriteNaturalNumbers(N-1);
+}
---
+++ solve_dz9/T66/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов 
+//в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+int M = Prompt("Задайте M ");
+int N = Prompt("Задайте N ");
+int tmp;
+if (M>N)
+{
+    tmp = M;
+    M=N;
+    N=tmp;
+}
+
+System.Console.Write(SumOfNumbersBetweenNandM(M, N));
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    return Convert.ToInt32(value);
+}
+
+int SumOfNumbersBetweenNandM(int M, int N)
+{
+    if (N==M) return N;
+    return N+= SumOfNumbersBetweenNandM(M, N-1);
+}
+
